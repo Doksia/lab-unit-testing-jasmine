6ce50c3288d571275d0753c4a62468317146a5b9
Miguel Romero <miguel.insti2@gmail.com>
2026-05-23 16:27:42 +0100

    solved lab

--- src/add.js
+++ src/add.js
@@ -1,7 +1,8 @@
 function add (numOne, numTwo) {
+    let numbers = []
     if (numOne == undefined || numTwo == undefined) {
         return undefined;
     }
-
+    add(2,3);
     return numOne + numTwo;
 }
--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,7 @@
-function divide () {
-       
+function divide (num1,num2) {
+    let numbers = []
+    if (num1 === undefined || num2 === undefined){
+        return undefined;
+    } add(4,2);
+       return num1/num2;
 }
